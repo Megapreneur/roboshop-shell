dd0d8433d08266a0060bf6742cd533de60f953c3
Megapreneur <ademolamegbabi@gmail.com>
2024-01-04 17:15:16 +0100

    mongodb server

--- mongodb.sh
+++ mongodb.sh
@@ -1,9 +1,9 @@
 ## Copy repo
 echo -e "\e[33mCopy MongoDB Repo File\e[0m"
-cp mongodb.repo /etc/yum.repos.d/mongodb.repo
+cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
 echo -e "\e[33mInstalling MongoDB Server\e[0m"
-dnf install mongodb-org -y
+dnf install mongodb-org -y &>>/tmp/roboshop.log
 ## Modify the config file
 echo -e "\e[33mStart MongoDB Server\e[0m"
-systemctl enable mongod
-systemctl restart mongod
+systemctl enable mongod &>>/tmp/roboshop.log
+systemctl restart mongod &>>/tmp/roboshop.log
