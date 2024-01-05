b4e39bf86e06046cc712735542909c0aef141dcd
Megapreneur <ademolamegbabi@gmail.com>
2024-01-05 14:15:19 +0100

    mongodb server

--- mongodb.sh
+++ mongodb.sh
@@ -4,6 +4,8 @@ cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
 echo -e "\e[33mInstalling MongoDB Server\e[0m"
 dnf install mongodb-org -y &>>/tmp/roboshop.log
 ## Modify the config file
+echo -e "\e[33mUpdate MongoDB Listen Address\e[0m"
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongodb.conf
 echo -e "\e[33mStart MongoDB Server\e[0m"
 systemctl enable mongod &>>/tmp/roboshop.log
 systemctl restart mongod &>>/tmp/roboshop.log
