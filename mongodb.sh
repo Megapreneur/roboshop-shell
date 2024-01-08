469aaeb8acfda41524f16f8f76f8cbf13276c3b1
Megapreneur <ademolamegbabi@gmail.com>
2024-01-08 12:15:24 +0100

    Roboshop Ngnix Server

--- mongodb.sh
+++ mongodb.sh
@@ -2,7 +2,7 @@
 echo -e "\e[33mCopy MongoDB Repo File\e[0m"
 cp /root/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
 echo -e "\e[33mInstalling MongoDB Server\e[0m"
-dnf install mongodb-org -y &>>/tmp/roboshop.log
+yum install mongodb-org -y &>>/tmp/roboshop.log
 ## Modify the config file
 echo -e "\e[33mUpdate MongoDB Listen Address\e[0m"
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
