2fbed88f6ba9657f4a322abbfe661af0320be0bc
Megapreneur <ademolamegbabi@gmail.com>
2024-01-08 19:34:46 +0100

    Mysql Server

--- mysql.sh
+++ mysql.sh
@@ -1,14 +1,16 @@
-
+echo -e "\e[33Configuring MySql\e[0m"
 dnf module disable mysql -y &>>/tmp/roboshop.log
 
-echo -e "\e[33mCopy MongoDB Repo File\e[0m"
+echo -e "\e[33mCopy MySql Repo File\e[0m"
 cp /root/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo &>>/tmp/roboshop.log
-dnf install mysql-community-server -y &>>/tmp/roboshop.log
 
-echo -e "\e[33mStart MongoDB Server\e[0m"
+echo -e "\e[33mInstall MySql Server\e[0m"
+yum install mysql-community-server -y &>>/tmp/roboshop.log
+
+echo -e "\e[33mStart MySql Server\e[0m"
 systemctl enable mysqld &>>/tmp/roboshop.log
 systemctl start mysqld &>>/tmp/roboshop.log
 
-echo -e "\e[33mChange Mysql Password\e[0m"
+echo -e "\e[33mChange MySql Password\e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1 &>>/tmp/roboshop.log
 
