d7a3e0a9537d9a5615a121606603438a668534c8
Megapreneur <ademolamegbabi@gmail.com>
2024-01-08 19:37:08 +0100

    Mysql Server

--- mysql.sh
+++ mysql.sh
@@ -1,5 +1,5 @@
-echo -e "\e[33Configuring MySql\e[0m"
-dnf module disable mysql -y &>>/tmp/roboshop.log
+#echo -e "\e[33Configuring MySql\e[0m"
+#dnf module disable mysql -y &>>/tmp/roboshop.log
 
 echo -e "\e[33mCopy MySql Repo File\e[0m"
 cp /root/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo &>>/tmp/roboshop.log
