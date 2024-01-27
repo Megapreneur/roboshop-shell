ff00afea7fbf8eb89e0193fd06f5afb3ead3a5b3
Megapreneur <ademolamegbabi@gmail.com>
2024-01-27 15:16:15 +0100

    Payment Script

--- payment.sh
+++ payment.sh
@@ -1,21 +1,21 @@
 
 echo -e "\e[33mInstall Python 3.6\e[0m"
-dnf install python36 gcc python3-devel -y
+dnf install python36 gcc python3-devel -y &>>/tmp/roboshop.log
 echo -e "\e[33mAdd Application User\e[0m"
-useradd roboshop
+useradd roboshop &>>/tmp/roboshop.log
 echo -e "\e[33mCreate Application Directory\e[0m"
-mkdir /app
+mkdir /app &>>/tmp/roboshop.log
 echo -e "\e[33mDownloading Application Content\e[0m"
-curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
+curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip &>>/tmp/roboshop.log
 echo -e "\e[33mExtract Application Content\e[0m"
 cd /app
-unzip /tmp/payment.zip
-cd /app
+unzip /tmp/payment.zip &>>/tmp/roboshop.log
+cd /app &>>/tmp/roboshop.log
 echo -e "\e[33mDownload Dependencies\e[0m"
-pip3.6 install -r requirements.txt
+pip3.6 install -r requirements.txt &>>/tmp/roboshop.log
 echo -e "\e[33mSetup Payment Service\e[0m"
 cp /root/roboshop-shell/payment.service /etc/systemd/system/payment.service &>>/tmp/roboshop.log
 echo -e "\e[33mStart Payment Service\e[0m"
-systemctl daemon-reload
-systemctl enable payment
-systemctl start payment
+systemctl daemon-reload &>>/tmp/roboshop.log
+systemctl enable payment &>>/tmp/roboshop.log
+systemctl start payment &>>/tmp/roboshop.log
