0802cd93086bdaed904b35d7cbad186ba7940b2a
Megapreneur <ademolamegbabi@gmail.com>
2024-01-28 16:54:14 +0100

    modifying gScript

--- dispatch.sh
+++ dispatch.sh
@@ -3,7 +3,7 @@ component=dispatch
 
 echo -e "${color}Install Golang${nocolor}"
 dnf install golang -y &>>${log_file}
-if [ $1 -eq 0 ]; then
+if [ $? -eq 0 ]; then
     echo SUCCESS
   else
     echo FAILURE
@@ -11,7 +11,7 @@ if [ $1 -eq 0 ]; then
 
 echo -e "${color}Add Application User${nocolor}"
 useradd roboshop &>>${log_file}
-if [ $1 -eq 0 ]; then
+if [ $? -eq 0 ]; then
     echo SUCCESS
   else
     echo FAILURE
@@ -19,7 +19,7 @@ if [ $1 -eq 0 ]; then
 
 echo -e "${color}Create Application Directory${nocolor}"
 mkdir ${app_path} &>>${log_file}
-if [ $1 -eq 0 ]; then
+if [ $? -eq 0 ]; then
     echo SUCCESS
   else
     echo FAILURE
@@ -27,7 +27,7 @@ if [ $1 -eq 0 ]; then
 
 echo -e "${color}Downloading Application Content${nocolor}"
 curl -L -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>${log_file}
-if [ $1 -eq 0 ]; then
+if [ $? -eq 0 ]; then
     echo SUCCESS
   else
     echo FAILURE
@@ -37,7 +37,7 @@ echo -e "${color}Extract Application Content${nocolor}"
 cd ${app_path} &>>${log_file}
 unzip /tmp/$component.zip &>>${log_file}
 cd ${app_path} &>>${log_file}
-if [ $1 -eq 0 ]; then
+if [ $? -eq 0 ]; then
     echo SUCCESS
   else
     echo FAILURE
@@ -46,7 +46,7 @@ if [ $1 -eq 0 ]; then
 echo -e "${color}Download Dependencies${nocolor}"
 go mod init $component &>>${log_file}
 go get &>>${log_file}
-if [ $1 -eq 0 ]; then
+if [ $? -eq 0 ]; then
     echo SUCCESS
   else
     echo FAILURE
@@ -54,7 +54,7 @@ if [ $1 -eq 0 ]; then
 
 echo -e "${color}Build Application${nocolor}"
 go build &>>${log_file}
-if [ $1 -eq 0 ]; then
+if [ $? -eq 0 ]; then
     echo SUCCESS
   else
     echo FAILURE
@@ -62,7 +62,7 @@ if [ $1 -eq 0 ]; then
 
 echo -e "${color}Setup Dispatch Service${nocolor}"
 cp /root/roboshop-shell/$component.service /etc/systemd/system/$component.service &>>${log_file}
-if [ $1 -eq 0 ]; then
+if [ $? -eq 0 ]; then
     echo SUCCESS
   else
     echo FAILURE
@@ -72,7 +72,7 @@ echo -e "${color}Start Dispatch Service${nocolor}"
 systemctl daemon-reload &>>${log_file}
 systemctl enable $component &>>${log_file}
 systemctl start $component &>>${log_file}
-if [ $1 -eq 0 ]; then
+if [ $? -eq 0 ]; then
     echo SUCCESS
   else
     echo FAILURE
