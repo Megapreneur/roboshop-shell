79fc3eff2f2bde793b0038bc3b60b7201a9b66b4
Megapreneur <ademolamegbabi@gmail.com>
2024-01-31 10:48:36 +0100

    learning

--- front.sh
+++ front.sh
@@ -1,17 +1,21 @@
-echo -e "\e[31mInstall Nginx\e[0m"
+color="\e[33m"
+nocolor="\e[0m"
+
+
+echo -e "${color}Install Nginx ${nocolor}"
 dnf install nginx -y &>>/tmp/roboshop.log
-echo -e "\e[31mEnable Nginx\e[0m"
+echo -e "${color}Enable Nginx${nocolor}"
 systemctl enable nginx &>>/tmp/roboshop.log
-echo -e "\e[31mStarting Nginx\e[0m"
+echo -e "${color}Starting Nginx${nocolor}"
 systemctl start nginx &>>/tmp/roboshop.log
-echo -e "\e[31mInstall Nginx\e[0m"
+echo -e "${color}Install Nginx${nocolor}"
 rm -rf /usr/share/nginx/html/* &>>/tmp/roboshop.log
-echo -e "\e[31mDownloading Frontend Content\e[0m"
+echo -e "${color}Downloading Frontend Content${nocolor}"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/roboshop.log
-echo -e "\e[31mExtracting Frontend Content\e[0m"
+echo -e "${color}Extracting Frontend Content${nocolor}"
 cd /usr/share/nginx/html &>>/tmp/roboshop.log
 unzip /tmp/frontend.zip &>>/tmp/roboshop.log
-echo -e "\e[31mCopying Roboshop Configuration File\e[0m"
+echo -e "${color}Copying Roboshop Configuration File${nocolor}"
 cp /root/roboshop-shell/roboshop.conf /etc//nginx/default.d/roboshop.conf &>>/tmp/roboshop.log
-echo -e "\e[31mRestart Nginx\e[0m"
+echo -e "${color}Restart Nginx${nocolor}"
 systemctl restart nginx &>>/tmp/roboshop.log
